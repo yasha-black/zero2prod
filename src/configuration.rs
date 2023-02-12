b00a5667c7de94bb3afed2ea75743acde5814c4e
yashablack <iamyashablack@gmail.com>
2023-02-12 15:08:48 +0300

    setup config

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,14 +1,24 @@
-#[derive(serde::Deserialize)]
+use config::{Config, ConfigError, File, FileFormat};
+
+#[derive(serde::Deserialize, Debug)]
 pub struct Settings {
     pub database: DatabaseSettings,
     pub application_port: u16,
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Debug)]
 pub struct DatabaseSettings {
+    pub name: String,
     pub username: String,
     pub password: String,
     pub port: u16,
     pub host: String,
-    pub database_name: String,
+}
+
+pub fn get_config() -> Result<Settings, ConfigError> {
+    let settings = Config::builder()
+        .add_source(File::new("configuration.yaml", FileFormat::Yaml))
+        .build();
+
+    settings?.try_deserialize::<Settings>()
 }
